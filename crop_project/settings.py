ef7b8456bf628206f0d19be0e858ec528cc38f9f
NarimanOusman <narimanrobo8@gmail.com>
2026-01-26 07:38:12 -0800

    feat: Initialize Django project settings and include the 'prediction' app.

--- crop_project/settings.py
+++ crop_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k#p-joeap7r=ziwl=8)nw9hwg+fvt7x$qci%!i&1(x5y_j*@7p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
